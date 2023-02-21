95b3ee5d1e41eba1e2ed46fe601e5a1e73fe31be
Jeff L <jeffreyliu97@gmail.com>
2023-02-20 22:46:38 -0800

    uydpate

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -5,3 +5,21 @@ from .auth_routes import validation_errors_to_error_messages
 
 
 card_routes = Blueprint('card', __name__)
+
+#blueprint registered with url prefix of /api/dashboard/cards
+
+@card_routes.route("/")
+@login_required
+def get_all_cards():
+# all cards must belong to a user or data should be invalid
+  all_cards = Card.query.filter(user_id=current_user.id).all()
+
+  output = {
+    "allCards": []
+  }
+
+  for card in all_cards:
+    info = card.to_dict()
+    output["allCards"].append(info)
+
+  return output
