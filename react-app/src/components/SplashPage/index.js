9085727c40d30ab42c8d6adb559d5e79bb091e63
Jeff L <jeffreyliu97@gmail.com>
2023-02-22 15:42:19 -0800

    lots of css on splashpage

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
+import "./SplashPage.css";
 
 const SplashPage = () => {
   const sessionUser = useSelector((state) => state.session.user);
@@ -12,7 +13,26 @@ const SplashPage = () => {
 
   return (
     <>
-      <div className="Splash-Container"></div>
+      <div className="Splash-Container">
+        <div className="Splash-Text-Image-Container">
+          <h1 className="Splash-Text">
+            The Next <br />
+            <span className="Splash-GenerationSpan">Generation</span>
+            Payment Method
+          </h1>
+          <div className="Splash-GetStarted">
+            <div>
+              Get <br />
+              Started
+            </div>
+            <i class="fa-solid fa-arrow-up"></i>
+          </div>
+          <img
+            src={process.env.PUBLIC_URL + "/robotarm.png"}
+            className="robotarm"
+          />
+        </div>
+      </div>
     </>
   );
 };
