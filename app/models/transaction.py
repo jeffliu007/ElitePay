0c197effd6e865c59d6060f1e6cc2cbf18c04c87
Jeff L <jeffreyliu97@gmail.com>
2023-02-21 20:58:24 -0800

    updated models again

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -12,7 +12,6 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float)
     description = db.Column(db.String(255))
-    status = db.Column(db.String(50))
     created_at = db.Column(db.DateTime, default=today)
     sender_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     recipient_id = db.Column(db.Integer, nullable=False)
@@ -25,7 +24,6 @@ class Transaction(db.Model):
         'id':self.id,
         'amount': self.amount,
         'description': self.description,
-        'status': self.status,
         'created_at': self.created_at,
         'sender_id': self.sender_id,
         "recipient_id": self.recipient_id,
--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -3,7 +3,6 @@ from app.models import db, Transaction, environment, SCHEMA
 transaction1 = Transaction(
   amount = 25.50,
   description = 'gas money thanks',
-  status = "pending",
   sender_id = 1,
   recipient_id = 2
 )
@@ -11,7 +10,6 @@ transaction1 = Transaction(
 transaction2 = Transaction(
   amount = 200.25,
   description = 'utility bill',
-  status = "pending",
   sender_id = 1,
   recipient_id = 3
 )
@@ -19,7 +17,6 @@ transaction2 = Transaction(
 transaction3 = Transaction(
   amount = 19.99,
   description = 'thank you for dinner!',
-  status = "pending",
   sender_id = 1,
   recipient_id = 2
 )
@@ -27,7 +24,6 @@ transaction3 = Transaction(
 transaction4 = Transaction(
   amount = 8.00,
   description = 'thanks for buying me snacks from the vending machine',
-  status = "pending",
   sender_id = 2,
   recipient_id = 1
 )
@@ -35,7 +31,6 @@ transaction4 = Transaction(
 transaction5 = Transaction(
   amount = 35.67,
   description = 'I owe you',
-  status = "pending",
   sender_id = 2,
   recipient_id = 3
 )
@@ -43,7 +38,6 @@ transaction5 = Transaction(
 transaction6 = Transaction(
   amount = 12.50,
   description = 'split check, paying you back',
-  status = "pending",
   sender_id = 3,
   recipient_id = 1
 )
