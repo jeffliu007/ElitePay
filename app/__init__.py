1419615adba7ad59e0611c16b18327d656eaf9a1
Jeff L <jeffreyliu97@gmail.com>
2023-02-21 21:52:45 -0800

    transaction routes started

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
 from .api.card_routes import card_routes
+from .api.transaction_routes import transaction_routes
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
@@ -30,6 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(card_routes, url_prefix='/api/dashboard/cards')
+app.register_blueprint(transaction_routes, url_prefix='/api/dashboard/transactions')
 db.init_app(app)
 Migrate(app, db)
 
