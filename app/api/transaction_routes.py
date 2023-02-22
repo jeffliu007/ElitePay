b53d69b1b6b8cd965dbf048c0261a81d8e173959
Jeff L <jeffreyliu97@gmail.com>
2023-02-21 22:22:10 -0800

    create transaction works

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
 from .auth_routes import validation_errors_to_error_messages
 from app.models import db, Transaction
+from ..forms.transaction_form import CreateTransactionForm
 
 transaction_routes = Blueprint('transaction', __name__)
 
@@ -22,3 +23,41 @@ def get_all_transactions():
       output['allTransactions'].append(transaction.to_dict())
 
   return output
+
+
+#route to get a single transaction
+@transaction_routes.route('/<int:transactionId>')
+@login_required
+def get_single_transaction(transactionId):
+  single_transaction = db.session.query(Transaction).get(int(transactionId))
+
+  if not single_transaction:
+    return {"message": "Card couldn't be found"}, 404
+
+  info = single_transaction.to_dict()
+
+  return info
+
+
+#route to create a new transaction
+@transaction_routes.route("/", methods=['POST'])
+@login_required
+def create_transaction():
+  form = CreateTransactionForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  if form.validate_on_submit():
+    data = form.data
+
+    new_transaction = Transaction(
+      amount = data['amount'],
+      description = data['description'],
+      sender_id = current_user.id,
+      recipient_id = data['recipient_id']
+    )
+
+    db.session.add(new_transaction)
+    db.session.commit()
+    return new_transaction.to_dict()
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 401
---
+++ app/forms/transaction_form.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import (
+StringField, IntegerField, FloatField
+)
+from wtforms.validators import DataRequired, ValidationError
+
+
+def amount_validator(form,field):
+  if field.data <= 0:
+    raise ValidationError("The amount entered must be greater than 0")
+
+def description_validator(form,field):
+  if len(field.data) > 100:
+    raise ValidationError("Description must be less than 255 characters long")
+
+class CreateTransactionForm(FlaskForm):
+  amount = FloatField('amount', validators=[DataRequired(), amount_validator])
+  description = StringField('description', validators=[DataRequired(), description_validator])
+  recipient_id = IntegerField('receiver', validators=[DataRequired()])
