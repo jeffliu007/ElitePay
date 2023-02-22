73d25ef0c856dd2c78cd8ea1f2c605628e2f2920
Jeff L <jeffreyliu97@gmail.com>
2023-02-22 13:52:00 -0800

    splash page start

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -4,4 +4,17 @@ import * as sessionActions from "../../store/session";
 const SplashPage = () => {
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
+
+  // const demoLogin = () => {
+  //   dispatch(sessionActions.login("demo@aa.io", "password"));
+  //   return history.push("/dashboard");
+  // };
+
+  return (
+    <>
+      <div className="Splash-Container"></div>
+    </>
+  );
 };
+
+export default SplashPage;
