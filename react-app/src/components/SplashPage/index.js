a1016cecdd38cdf3b8cfcf9428917904801abefe
Jeff L <jeffreyliu97@gmail.com>
2023-02-22 16:52:01 -0800

    so much tedious css on splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,20 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
+import { useHistory } from "react-router-dom";
 import "./SplashPage.css";
 
 const SplashPage = () => {
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   // const demoLogin = () => {
   //   dispatch(sessionActions.login("demo@aa.io", "password"));
-  //   return history.push("/dashboard");
+  //   return history.push("/api/dashboard/cards");
   // };
 
   return (
     <>
       <div className="Splash-Container">
         <div className="Splash-Text-Image-Container">
+          <i class="fa-solid fa-arrow-up"></i>
+          <img
+            src={process.env.PUBLIC_URL + "/logo.png"}
+            className="Splash-Logo"
+          />
           <h1 className="Splash-Text">
             The Next <br />
             <span className="Splash-GenerationSpan">Generation</span>
@@ -25,12 +32,15 @@ const SplashPage = () => {
               Get <br />
               Started
             </div>
-            <i class="fa-solid fa-arrow-up"></i>
           </div>
-          <img
-            src={process.env.PUBLIC_URL + "/robotarm.png"}
-            className="robotarm"
-          />
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + "/robotarm.png"}
+              className="robotarm"
+            />
+            <div className="Splash-Background-Gradient" />
+          </div>
+          <div className="Splash-Background-Gradient" />
         </div>
       </div>
     </>
