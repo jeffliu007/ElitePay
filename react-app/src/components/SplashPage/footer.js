186e9ca275fe3e200c2da2858ee59bbec6eaf963
Jeff L <jeffreyliu97@gmail.com>
2023-04-01 20:25:25 -0700

    update footer

--- react-app/src/components/SplashPage/footer.js
+++ react-app/src/components/SplashPage/footer.js
@@ -11,27 +11,42 @@ const Footer = () => {
 
   return (
     <div className="SplashPage-Footer">
-      {footer_apps.map((app) => {
-        return (
-          <div className="SplashPage-Footer-Text" key={app}>
-            {app}
-          </div>
-        );
-      })}
-      <a
-        href="https://github.com/jeffliu007"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa-brands fa-github"></i>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/jeffreyliu17/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa-brands fa-linkedin"></i>
-      </a>
+      <div className="SplashPage-Top">
+        {footer_apps.map((app) => {
+          return (
+            <div className="SplashPage-Footer-Text" key={app}>
+              {app}
+            </div>
+          );
+        })}
+        <a
+          href="https://github.com/jeffliu007"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jeffreyliu17/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+      <div className="SplashPage-Bottom">
+        All Rights Reserved:{" "}
+        <span>
+          <a
+            href="https://jeffliu007.github.io/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Jeff Liu
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
